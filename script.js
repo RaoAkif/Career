9e6f4bd21c67183291d27d1c58466c2b52825f11
raoakif <akifrao@gmail.com>
2023-01-02 23:09:36 +0500

    Add Leetcode Daily and Easy Apply Jobs Links

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ save = function () {
   var list = document.querySelectorAll(`[type*="checkbox"]`);
   list.forEach((el) => {
     localStorage.setItem(el.id, el.checked);
-    console.log(el.id, el.checked);
+    // console.log(el.id, el.checked);
   });
 };
